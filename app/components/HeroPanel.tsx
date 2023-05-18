824476f4b0cad02581b3fea912b2d764233949b6
unknown <coderanirudh@gmail.com>
2023-05-18 11:43:45 +0530

    Hero panel added, API can be added later for location

---
+++ app/components/HeroPanel.tsx
@@ -0,0 +1,23 @@
+'use client'
+import Image from 'next/image'
+// import Video from '../../public/videos/'
+
+const HeroPanel = () => {
+  return (
+<div className="h-[90vh] w-screen relative">
+  <div className="VideoHolder h-full w-full top-0 right-0 left-0 bottom-0 relative">
+    <video className="fixed h-full w-full object-cover" autoPlay loop preload="auto" muted>
+      <source src="/videos/bgVideo.mp4" type="video/mp4" />
+    </video>
+    <div className="absolute top-0 left-0 w-full h-full z-10 flex flex-col justify-center items-center">
+      <h1 className="text-sky-400 text-6xl font-bold mb-8">REDUCE SUPPLY CHAIN COSTS. </h1>
+      <h1 className="text-white text-6xl font-bold">WITH HUMAN CENTRIC AI IN USA </h1>
+      <button className="rounded-full p-4 mt-10 primary bg-sky-600">Get in Touch ➤ </button>
+    </div>
+    <div className="absolute top-0 left-0 w-full h-full bg-black opacity-60" />
+  </div>
+</div>
+  )
+}
+
+export default HeroPanel
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Navbar from './components/Navbar'
+import HeroPanel from './components/HeroPanel'
 
 export default function Home() {
   return (
 <div className="text-xl"> 
 <Navbar/>
+<HeroPanel/>
 </div>    
   )
 }
